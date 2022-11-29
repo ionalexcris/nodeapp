9b9b483eb2516469f70082609afba5af24b230b1
ionalexcris <ion.alex1798@gmail.com>
2022-11-29 10:48:50 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
+
+const postRouter = require("./routes/postRoutes")
+
+const app = express()
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+const connectWithRetry = () => {
+    mongoose
+     .connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        
+     })
+     .then(() => console.log("succesfully connected to DB"))
+     .catch((e) => {
+        console.log(e)
+        setTimeout(connectWithRetry, 5000)
+     });
+}
+
+connectWithRetry();
+
+app.use(express.json());
+
+app.get("/", (req,res) => {
+    res.send("<h2>Express  APP  2.0!!!!</h2>")
+})
+
+
+app.use("/api/v1/posts",postRouter)
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=> console.log(`listening on port ${port}`))
